e1f58c849b623bd9192d9c51c1880e3efa875553
Kalina.A <Kalina.A@digdes.com>
2017-08-08 18:32:24 +0300

    RevisionManager added

---
+++ LibreOfficeLibrary/RevisionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LibreOfficeLibrary
+{
+	public class RevisionManager
+	{
+		public void AcceptAllRevisions(string filePath)
+		{
+			if (!File.Exists(filePath))
+				throw new ArgumentException("The file doesn't exist");
+
+			var tempDirPath = Path.GetTempPath().TrimEnd(Path.DirectorySeparatorChar);
+			var tempFilePath = Path.Combine(tempDirPath, Path.GetRandomFileName());
+			File.Copy(filePath, tempFilePath);
+
+			var worker = new LibreOfficeWorker();
+			worker.DoWork($"macro:///LibreOfficeLibrary.Module1.AcceptAllChanges(\"{tempFilePath}\")");
+
+			File.Delete(filePath);
+			File.Move(tempFilePath, filePath);
+		}
+	}
+}
