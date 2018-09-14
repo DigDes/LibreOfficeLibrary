58e8c3a1c0e7d45299403c42ce73306a45d03b89
DIGDES\Kirillov.D <Kirillov.D@digdes.com>
2018-09-14 11:08:19 +0300

    added UserInstallation parameter

--- LibreOfficeLibrary/DocumentConverter.cs
+++ LibreOfficeLibrary/DocumentConverter.cs
@@ -14,21 +14,33 @@ namespace LibreOfficeLibrary
 		/// <summary>
 		/// Convert document to PDF format
 		/// </summary>
-		public void ConvertToPdf(string filePath, string targetPath)
+		public void ConvertToPdf(string filePath, string targetPath, string profileLocation = null)
 		{
 			if (!File.Exists(filePath))
 				throw new ArgumentException("The file doesn't exist");
 			if (File.Exists(targetPath))
 				throw new ArgumentException("The target file exists");
 
+			if (profileLocation == null)
+			{
+				var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+				var versionDir = Directory.GetDirectories(Path.Combine(path, "libreoffice")).FirstOrDefault();
+				if(versionDir==null)
+					return;
+
+				profileLocation = versionDir;
+			}
+
+			var profilePath = Path.GetFullPath(profileLocation).Replace('\\', '/').Replace(" ", "%20");
 			var tempDirPath = Path.GetTempPath().TrimEnd(Path.DirectorySeparatorChar);
 			var tempFilePath = Path.Combine(tempDirPath, Path.GetRandomFileName());
 			var tempOutputFilePath = Path.Combine(tempDirPath, Path.GetFileNameWithoutExtension(tempFilePath) + ".pdf");
 
 			File.Copy(filePath, tempFilePath);
 
+
 			var worker = new LibreOfficeWorker();
-			worker.DoWork($"/C -headless -writer -convert-to pdf -outdir \"{tempDirPath}\" \"{tempFilePath}\"");
+			worker.DoWork($"/C -headless -writer -convert-to pdf -outdir \"{tempDirPath}\" \"{tempFilePath}\" \"-env:UserInstallation=file:///{profilePath}/\"");
 
 			if (File.Exists(tempOutputFilePath))
 				File.Move(tempOutputFilePath, targetPath);
