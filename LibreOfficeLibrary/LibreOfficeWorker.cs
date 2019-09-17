fc438b2f770ace0d22c50fedfb55d689c56dadec
DIGDES\Mezentsev.R <Mezentsev.R@digdes.com>
2019-09-17 15:39:30 +0300

    Added timeout param, catch thread abort, linux support

--- LibreOfficeLibrary/LibreOfficeWorker.cs
+++ LibreOfficeLibrary/LibreOfficeWorker.cs
@@ -9,9 +9,9 @@ namespace LibreOfficeLibrary
 {
 	public class LibreOfficeWorker
 	{
-		private const int TimeForWaiting = 600;
 		private class Worker
 		{
+			public string ErrorMessage { get; private set; }
 			public Worker(object parameterObject)
 			{
 				Process = new Process
@@ -19,35 +19,88 @@ namespace LibreOfficeLibrary
 					StartInfo = new ProcessStartInfo
 					{
 						WindowStyle = ProcessWindowStyle.Hidden,
-						FileName = "soffice.exe",
+						FileName = GetLibreofficeAppName(),
 						Arguments = (string)parameterObject
 					}
 				};
 			}
 			public void DoWork()
 			{
-				using (Process)
+				ErrorMessage = string.Empty;
+				try
 				{
-					Process.Start();
-					Process.WaitForExit();
+					using (Process)
+					{
+						Process.Start();
+						Process.WaitForExit();
+					}
+				}
+				catch (Exception e)
+				{
+					ErrorMessage = e.ToString();
 				}
 			}
 			public Process Process { get; }
 		}
-		public void DoWork(string argumentString)
+		public void DoWork(string argumentString, int? timeForWaiting = null)
 		{
 			var worker = new Worker(argumentString);
-			var thread = new Thread(worker.DoWork);
-			thread.IsBackground = true;
+			var thread = new Thread(worker.DoWork)
+			{
+				IsBackground = true
+			};
 			thread.Start();
 
-			if (!thread.Join(TimeSpan.FromSeconds(TimeForWaiting)))
+			if (timeForWaiting.HasValue && !thread.Join(TimeSpan.FromSeconds(timeForWaiting.Value)))
 			{
 				worker.Process.Kill();
 				worker.Process.Dispose();
 				thread.Abort();
 				throw new ConvertDocumentException("LibreOffice process didn't respond within the expected time");
 			}
+
+			if (timeForWaiting.HasValue)
+				return;
+
+			try
+			{
+				thread.Join();
+			}
+			catch (ThreadAbortException)
+			{
+				worker.Process.Kill();
+				worker.Process.Dispose();
+				thread.Abort();
+				throw;
+			}
+			catch (Exception)
+			{
+				if (thread.IsAlive)
+					thread.Abort();
+			}
+
+			if (!string.IsNullOrEmpty(worker.ErrorMessage))
+			{
+				throw new ConvertDocumentException($"LibreOffice process error {worker.ErrorMessage}");
+			}
+		}
+
+		private static bool IsLinux()
+		{
+#if NET461
+			return false;
+#elif NETSTANDARD2_0
+			return System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux);
+#endif
+		}
+
+		private static string GetLibreofficeAppName()
+		{
+#if NET461
+			return "soffice.exe";
+#elif NETSTANDARD2_0
+			return IsLinux() ? "libreoffice" : "soffice.exe";
+#endif
 		}
 	}
 }
